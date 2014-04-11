b953ff095e262bd69c3147fcc10222e04ce1c797
Sean Cullen <scullen@creativee.net>
2014-04-11 21:04:50 +0000

    New Karma application

---
+++ app/views/goals/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @goal, :id, :name, :motivation, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Karma::Application.config.secret_key_base = '7aa48f4ea9561fb41c475c4c793b0b13ee728b16def1072da907d5110d75f8b923982d1a22e0c26007694f1a80bfbeaf54edff44148abd4f3980de22815b2528'
